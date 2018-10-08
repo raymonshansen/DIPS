fe1abdfb601fb69e4a2b4be1380c333725f347b4
Raymon Hansen <raymon.s.hansen@gmail.com>
2018-10-08 12:26:43 +0200

    Add. Test for TodoList.

--- todo_test.py
+++ todo_test.py
@@ -3,15 +3,23 @@ from todo import Task, TodoList
 
 
 class Test_TodoList(TestCase):
-    ...
+    def test_first_list_is_empty(self):
+        """A new TodoList should not have any tasks."""
+        new = TodoList()
+        self.assertEqual(new.next_id, 0)
+        self.assertEqual(new.tasks, list())
 
 
 class Test_Task(TestCase):
     def test_new_task_must_have_positive_ID(self):
         """A new task must have a positive ID."""
-        new = Task("New message")
-        self.assertGreaterEqual(new.id, 1, "New message does not have positive ID.")
+        self.assertRaises(ValueError, lambda: Task("New message.", -1))
 
     def test_task_must_have_content(self):
         """A new task must have content."""
-        self.assertRaises(ValueError, lambda: Task(None))
+        self.assertRaises(ValueError, lambda: Task(None, 1))
+
+    def test_new_task_is_uncompleted(self):
+        """A new task is uncompleted."""
+        new = Task("New message", 1)
+        self.assertFalse(new.completed)
