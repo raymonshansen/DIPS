cd4d061b9adb12f950989df26c487f8e1104e81d
Raymon Hansen <raymon.s.hansen@gmail.com>
2018-10-09 14:22:58 +0200

    Add. Functioning shell with commands in a dictionary.

--- todo_app.py
+++ todo_app.py
@@ -1,21 +1,27 @@
-from todo import TodoList, Task
+from todo import TodoList, Persister
 
 
 class Shell:
     """Runs the shell."""
     def __init__(self):
-        self.todolist = TodoList()
-        self.commands = {'add': self.todolist.add, 'do': self.todolist.do}
+        self.persister = Persister('tasks')
+        self.todolist = TodoList(self.persister)
+        self.commands = {'add': self.todolist.add,
+                         'do': self.todolist.do,
+                         'print': self.todolist.print_all_tasks}
 
     def print_welcome_message(self):
         """Display simple usage and how many
         messages are currently in the list, if any."""
-        msg1 = "Welcome to TODO-app!\n"
-        msg2 = "Add entry:\nadd <message>\n"
-        msg3 = "Mark as completed:\ndo <number of completed message>\n"
-        msg4 = "Display all entries:\nprint\n\n"
-        msg5 = f"You currently have {len(self.todolist)} entries.\n"
-        print(msg1 + msg2 + msg3 + msg4 + msg5)
+        welcome_message = f"""Welcome to TODO-app!
+Add entry:
+add <message>
+Mark as completed:
+do <number of completed message>
+Display all entries:
+print
+You currently have {len(self.todolist)} entries."""
+        print(welcome_message)
 
     def run(self):
         """Run the shell loop until we get 'exit'."""
@@ -23,12 +29,16 @@ class Shell:
         res = ""
         while res != "exit":
             res = input(" > ")
-            if res != "exit":
+            if res and res != "exit":
                 self.run_command(res)
 
     def run_command(self, input_str: str):
-        if input_str == "test":
-            print("This is a response!")
+        cmd, *args = input_str.split()
+        ret_string = self.commands.get(cmd, self.print_default_error)(" ".join(args))
+        print(ret_string)
+
+    def print_default_error(self, *args):
+        return "Error, unknown command."
 
 
 if __name__ == "__main__":
